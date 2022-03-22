1694e8d6f4eedf6318c5bdf820e6873b9f290aa1
shathakh <shathaqudaih17@gmail.com>
2022-03-22 22:24:05 +0200

    create search function includes fetch api with endpoint input value relates #39

--- category-search-page/category-search-page.js
+++ category-search-page/category-search-page.js
@@ -43,59 +43,37 @@ let title=document.querySelector(".title")
         localStorage.setItem("CardId", CardId);
       }
 
-      let items = [];
-      let returnMealsArray = [];
-      
-      function mealsArray() {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?c=${categoryName}`)
-          .then((res) => {
-            return res.json();
-          })
+      function searchMeal(e) {
+        e.preventDefault()
+        let userInput = input.value
+        console.log(userInput);
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+          .then((res) => res.json())
           .then((data) => {
-            data.meals.forEach((res) => {
-              let aForMeals = document.createElement("a");
-              aForMeals.classList.add("Card");
-              container.appendChild(aForMeals);
-      
-              let imgForMeals = document.createElement("img");
-              imgForMeals.src = res.strMealThumb;
-              aForMeals.appendChild(imgForMeals);
-      
-              let h3ForMeals = document.createElement("h3");
-              h3ForMeals.innerText = res.strMeal;
-              aForMeals.appendChild(h3ForMeals);
-            });
-            saveTolocalStorageArray(data.meals);
-          });
-      }
-      mealsArray();
-      returnMealsArray = JSON.parse(localStorage.getItem("MealsArray"));
-      
-      function searchData(value) {
-        container.innerHTML = "";
-        for (let i = 0; i < returnMealsArray.length; i++) {
-          if (returnMealsArray[i].strMeal.includes(value.toLowerCase())) {
-              returnMealsArray.forEach((res) => {
-                  let aForMeals = document.createElement("a");
-                  aForMeals.classList.add("Card");
-                  container.appendChild(aForMeals);
-      
-                  let imgForMeals = document.createElement("img");
-                  imgForMeals.src = res.strMealThumb;
-                  aForMeals.appendChild(imgForMeals);
-          
-                  let h3ForMeals = document.createElement("h3");
-                  h3ForMeals.innerText = res.strMeal;
-                  aForMeals.appendChild(h3ForMeals);
-      
-                  console.log("fffff",res.strMeal);
-      
-                });
-          }
-        }
-      }
-      
-      function saveTolocalStorageArray(item) {
-        localStorage.setItem("MealsArray", JSON.stringify(item));
+            let allData = data.meals
+            allData.forEach(ele => {
+              if(ele.strCategory === categoryName){
+                let card = document.createElement('div')
+            card.setAttribute('class','card')
+            container.appendChild(card)
+
+            let link = document.createElement('a')
+            card.appendChild(link)
+            link.addEventListener("click", () => {
+                location.href = '../recipe-page/recipe-page.html'
+                saveTolocalStorageCategory(meal.idMeal);
+              });
+    
+            let img= document.createElement('img')
+            img.src= meal.strMealThumb
+            link.appendChild(img)
+    
+            let nameMeal= document.createElement('h3')
+            nameMeal.textContent= meal.strMeal
+            link.appendChild(nameMeal)
+            input.value=" ";
+              }
+            })
+          })
       }
       
