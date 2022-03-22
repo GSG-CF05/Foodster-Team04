ed35fabe888bd0320d5bb28c36130349255d364a
shathakh <shathaqudaih17@gmail.com>
2022-03-22 13:42:46 +0200

    add event listener to the search button and fetch api to search for different desserts relates #29

--- desserts-recipes/desserts-recipes.js
+++ desserts-recipes/desserts-recipes.js
@@ -3,6 +3,9 @@ let container = document.querySelector('.container')
 let input= document.querySelector('.input-value')
 let buttonSearch= document.querySelector('.butt-search')
 
+
+buttonSearch.addEventListener('click', searchValue)
+
 // fetch food api with end point dessert category 
 fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=dessert`).then(response => response.json())
 .then((data)=>{
@@ -39,4 +42,42 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=dessert`).then(respo
 // function to save id dessert in local storage
 function saveTolocalStorageCategory(CardId) {
     localStorage.setItem("CardId", CardId);
+  }
+
+ 
+
+  function searchValue(e) {
+    e.preventDefault()
+    let value = input.value
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        let allData = data.meals
+        // console.log(allData);
+        allData.forEach((element) => {
+          if (element.strCategory === 'Dessert') {
+            console.log(element)
+            let card = document.createElement('div')
+            card.setAttribute('class', 'card')
+            container.appendChild(card)
+  
+            let link = document.createElement('a')
+            card.appendChild(link)
+  
+            let img = document.createElement('img')
+            img.src = element.strMealThumb
+            link.appendChild(img)
+  
+            link.addEventListener('click', () => {
+              location.href = '../recipe-page/recipe-page.html'
+              saveTolocalStorageCategory(element.idMeal)
+            })
+  
+            let nameMeal = document.createElement('h3')
+            nameMeal.textContent = element.strMeal
+            link.appendChild(nameMeal)
+            input.value = ' '
+          }
+        })
+      })
   }
