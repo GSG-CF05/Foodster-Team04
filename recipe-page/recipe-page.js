59a397e0c1d2f30799ac55692fbd7aa05021decc
AjShatali <abdulqader.shatali@gmail.com>
2022-03-22 12:40:17 +0200

    both popups are finished with their respective close functions #22

--- recipe-page/recipe-page.js
+++ recipe-page/recipe-page.js
@@ -1,28 +1,32 @@
-var recipePopup = document.getElementById("popup1");
-var recipeButton = document.getElementById("recipe-details-button");
-var span =  document.getElementsByClassName("close")[0];
-recipeButton.onclick = function(){
-    recipePopup.style.display = "block";
-    }
-span.onclick = function(){
-    recipePopup.style.display = "none";
+let recipePopup = document.getElementById('popup1')
+let recipeButton = document.getElementById('recipe-details-button')
+let span1 = document.getElementsByClassName('close')[0]
+
+recipeButton.onclick = function () {
+  recipePopup.style.display = 'block'
 }
-window.onclick = function(event){
-    if(event.target == recipePopup){
-        recipePopup.style.display="none";
-    } 
+span1.onclick = function () {
+  recipePopup.style.display = 'none'
 }
-var ingredientsPopup = document.getElementById("popup2");
-var ingredientsButton = document.getElementById("ingredients-button");
-var span =  document.getElementsByClassName("close");
-ingredientsButton.onclick = function(){
-    ingredientsPopup.style.display = "block";
-    }
-span.onclick = function(){
-   ingredientsPopup.style.display = "none";
+
+// Second popUp Starts here 
+
+let ingredientsPopup = document.getElementById('popup2')
+let ingredientsButton = document.getElementById('ingredients-button')
+let span2 = document.getElementsByClassName('close')[1]
+
+ingredientsButton.onclick = function () {
+  ingredientsPopup.style.display = 'block'
 }
-window.onclick = function(event){
-    if(event.target == ingredientsPopup){
-        ingredientsPopup.style.display="none";
-    } 
+span2.onclick = function () {
+  ingredientsPopup.style.display = 'none'
+}
+
+window.onclick = function (event) {
+  if (event.target == recipePopup) {
+    recipePopup.style.display = 'none'
+  }
+  if (event.target == ingredientsPopup) {
+    ingredientsPopup.style.display = 'none'
+  }
 }
